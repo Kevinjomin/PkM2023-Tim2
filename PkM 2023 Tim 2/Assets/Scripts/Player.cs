9123121a7bd3bd147c3b036d43c05fe1b6ccf748
Kevinjomin <kevin.jonathan.mintaryo@gmail.com>
2023-06-23 11:26:44 +0700

    added collider

--- PkM 2023 Tim 2/Assets/Scripts/Player.cs
+++ PkM 2023 Tim 2/Assets/Scripts/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
-
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -29,7 +28,41 @@ public class Player : MonoBehaviour
         }
         inputVector = inputVector.normalized;
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        // change playerSize based on the scale of player visual mesh
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 1f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        
+        if (!canMove) // when player cannot move
+        {
+            // attempt movement on x axis
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+            if (canMove)
+            {
+                moveDirection = moveDirectionX; //move only on x axis
+            }
+            else
+            {
+                // attempt movement on z axis
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ; //move only on z axis
+                }
+                else
+                {
+                    //cannot move in all direction
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
 
         // rotate animation
         float rotateSpeed = 8f;
