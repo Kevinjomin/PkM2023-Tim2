4c825c03cfaa7ccf8225d3d3016e702f9aa66bf1
Zakheus <134615674+SebastianPutraC@users.noreply.github.com>
2023-06-24 17:06:35 +0700

    Fix and Spawner Scripts

--- PkM 2023 Tim 2/Assets/Scripts/Colletibles/Collectibles.cs
+++ PkM 2023 Tim 2/Assets/Scripts/Colletibles/Collectibles.cs
@@ -6,10 +6,30 @@ using UnityEngine.UI;
 [System.Serializable]
 public class Collectibles
 {
+    public enum Rarity
+    {
+        COMMON,
+        UNCOMMON,
+        RARE
+    }
+    public enum Type
+    {
+        ORGANIC,
+        INORGANIC,
+        ELECTRONIC
+    }
     public int id;
+
     public string itemName;
     public string itemDesc;
-    public bool collected;
+
+    public Rarity rarity;
+    public Type type;
+
     public Sprite lockedImage;
     public Sprite unlockedImage;
+
+    public bool collected;
+
+    public GameObject mapObject;
 }
