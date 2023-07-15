258861b354286610186cd9fa8ed412972da28289
Zakheus <134615674+SebastianPutraC@users.noreply.github.com>
2023-07-15 17:22:04 +0700

    save slot and ui

---
+++ PkM 2023 Tim 2/Assets/Scripts/Save/SaveSlot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSlot : MonoBehaviour
+{
+    [SerializeField] private ProfileData profileData;
+    public string ownId;
+
+}
