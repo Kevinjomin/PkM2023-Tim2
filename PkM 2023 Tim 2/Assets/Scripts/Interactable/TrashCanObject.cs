175ec24d836dd81a1c00afeb2687bd30faf6f955
Kevinjomin <kevin.jonathan.mintaryo@gmail.com>
2023-07-26 10:34:48 +0700

    added box assets

--- PkM 2023 Tim 2/Assets/Scripts/Interactable/TrashCanObject.cs
+++ PkM 2023 Tim 2/Assets/Scripts/Interactable/TrashCanObject.cs
@@ -8,22 +8,27 @@ public class TrashCanObject : MonoBehaviour
     [SerializeField] public int typeIndex;
     [SerializeField] public bool isFilled = false;
     [SerializeField] public bool isCorrect = false;
+    private GameObject openMesh;
+    private GameObject closedMesh;
     /*
     Cardboard Box type index :
     - organic = 1
     - non organic = 2
     - B3 = 3
     */
+    private void Start()
+    {
+        openMesh = transform.Find("Visuals/Open").gameObject;
+        closedMesh = transform.Find("Visuals/Closed").gameObject;
+    }
 
     private void Update()
     {
-        // when highlight is active
-        if (HighlightVisual != null && HighlightVisual.activeSelf)
+        if (isFilled && openMesh.activeSelf)
         {
-            
-            //HighlightVisual.SetActive(false);
+            openMesh.SetActive(false);
+            closedMesh.SetActive(true);
         }
-
     }
 
     private void OnCollisionEnter(Collision collision)
